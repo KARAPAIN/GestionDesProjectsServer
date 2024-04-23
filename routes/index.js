3520ed225c2699ba89587e08dac57115a696833c
anbouri <anbouri@yandex.com>
2024-04-23 12:33:27 +0100

    Add test endpoint at root path

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ import taskRoutes from "./taskRoutes.js";
 
 const router = express.Router();
 
+// Define route handler for the root path
+router.get("/", (req, res) => {
+  // Define JSON data to be returned
+  const jsonData = {
+    message: "Backend is working fine!",
+    timestamp: new Date().toISOString(),
+  };
+
+  // Return JSON response
+  res.json(jsonData);
+});
+
 router.use("/user", userRoutes); //api/user/login
 router.use("/task", taskRoutes);
 
